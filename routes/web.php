8d7ed3e31b50cdf3c3a96e2ff2100e6cf8d153f9
Aldo ratmawan <aldo.ratmawan9999@gmail.com>
2023-07-10 21:54:09 +0700

    Change Server Application

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Events\EventResetPassword;
 use App\Http\Controllers\Web\VerificationsEmail;
-use App\Jobs\JobEmailMangAdmin;
-use App\Mail\MailForgotPassword;
-use App\Models\Admin\AdminMangdropship;
-use App\Models\PasswordAuthentications;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('NoticeSubscribtions', ['route' => 'akdioanicnaic']);
@@ -21,4 +16,9 @@ Route::controller(VerificationsEmail::class)->group(function(){
     Route::get('emails/verify', 'Verify')
         ->name('verify.email')
         ->middleware('handle_verify_email');
+});
+
+Route::get('test-oss', function(){
+    $url = Storage::disk('oss')->url('storage/kCb4ZVtIEk2MMi2yYS7eN2RbDxLpYBIiaTvyw2LR.png');
+    echo $url;
 });
